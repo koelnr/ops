6d11afc404a7e43040f012b88c111087de22bcd1
harshsandhu44 <hsandhu@speareducation.com>
2026-04-03 18:27:59 +0530

    feat: add clerk provider to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
-import { Google_Sans_Code, Space_Grotesk } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Sidebar } from "@/components/layout/sidebar";
 import { TopBar } from "@/components/layout/top-bar";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-sans",
 });
-
-// const googleSansCode = Google_Sans_Code({
-//   subsets: ["latin"],
-//   variable: "--font-google-sans-code",
-// });
 
 export const metadata: Metadata = {
   title: "Ops Dashboard",
@@ -32,18 +28,19 @@ export default function RootLayout({
       className={cn(
         "h-full",
         "antialiased",
-        // googleSansCode.variable,
         "font-sans",
         spaceGrotesk.variable,
       )}
     >
       <body className="h-full flex">
-        <Sidebar />
-        <div className="flex flex-1 flex-col ml-60 h-full overflow-hidden">
-          <TopBar />
-          <main className="flex-1 overflow-y-auto">{children}</main>
-        </div>
-        <Toaster richColors position="bottom-right" />
+        <ClerkProvider>
+          <Sidebar />
+          <div className="flex flex-1 flex-col ml-60 h-full overflow-hidden">
+            <TopBar />
+            <main className="flex-1 overflow-y-auto">{children}</main>
+          </div>
+          <Toaster richColors position="bottom-right" />
+        </ClerkProvider>
       </body>
     </html>
   );
